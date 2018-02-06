532e57fcb7222538978d049482cff86140f4b37e
Good-Pudge <arsenylyashenko.3@gmail.com>
2018-02-06 15:09:53 +0300

    Fix multi-split function. Enable all compiler warnings. Trying to fix reporter.

--- lib/util/string.cpp
+++ lib/util/string.cpp
@@ -23,16 +23,28 @@ namespace util {
         std::vector<std::string> split(std::string s, const std::vector<std::string> &delimiters) {
             std::vector<std::string> tokens;
 
-            while(!s.empty()) {
-                auto min_offset = std::string::npos;
-                std::string::size_type current_offset;
+            while(true) {
+                auto offset = std::string::npos;
+                std::string::size_type delim_size = 0;
                 for (const auto &delimiter : delimiters) {
-                    current_offset = s.find(delimiter);
-                    if (current_offset < min_offset) min_offset = current_offset;
+                    auto current_offset = s.find(delimiter);
+                    if (current_offset < offset) {
+                        offset = current_offset;
+                        delim_size = delimiter.length();
+                    }
                 }
-                tokens.emplace_back(s, s.length() - min_offset);
-                s.erase(0, min_offset);
+
+                if(offset == std::string::npos) {
+                    break;
+                }
+
+                std::string token(s, 0, offset);
+                if(!token.empty()) tokens.push_back(token);
+
+                s.erase(0, offset + delim_size);
             }
+
+            if(!s.empty()) tokens.push_back(s);
 
             return tokens;
         }
